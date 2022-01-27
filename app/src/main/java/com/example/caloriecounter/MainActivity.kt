e6896cc2ea51fb4b2d6ea8a8a4a6d22b5835b44e
Hrishikesh6419 <hrishikesh7165@gmail.com>
2022-01-27 00:15:06 -0500

    CC4 - Welcome Screen and Uitility (#4)
    Co-authored-by: Hrishikesh <hrishikesh.dharshan@citizensbank.com>

--- app/src/main/java/com/example/caloriecounter/MainActivity.kt
+++ app/src/main/java/com/example/caloriecounter/MainActivity.kt
@@ -9,16 +9,14 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.caloriecounter.ui.theme.CalorieCounterTheme
+import com.example.onboarding_presentation.welcome.WelcomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CalorieCounterTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                WelcomeScreen()
             }
         }
     }
--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ buildscript {
         classpath(Build.kotlinGradlePlugin)
         classpath(Build.ktlintAndroidGradlePlugin)
         classpath(Build.detektAndroidGradlePlugin)
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
