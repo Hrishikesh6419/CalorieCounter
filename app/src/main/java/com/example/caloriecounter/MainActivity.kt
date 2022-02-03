1868120d91e35a3c8aa0854f3aadf7d787a1db61
Hrishikesh6419 <hrishikesh7165@gmail.com>
2022-02-02 22:43:16 -0500

    CC9 - Age Screen (#9)
    Co-authored-by: Hrishikesh <hrishikesh.dharshan@citizensbank.com>

--- app/src/main/java/com/example/caloriecounter/MainActivity.kt
+++ app/src/main/java/com/example/caloriecounter/MainActivity.kt
@@ -3,8 +3,12 @@ package com.example.caloriecounter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -12,6 +16,7 @@ import androidx.navigation.compose.rememberNavController
 import com.example.caloriecounter.navigation.navigate
 import com.example.caloriecounter.ui.theme.CalorieCounterTheme
 import com.example.core.navigation.Route
+import com.example.onboarding_presentation.age.AgeScreen
 import com.example.onboarding_presentation.gender.GenderScreen
 import com.example.onboarding_presentation.welcome.WelcomeScreen
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,41 +28,50 @@ class MainActivity : ComponentActivity() {
         setContent {
             CalorieCounterTheme {
                 val navController = rememberNavController()
-                NavHost(
-                    navController = navController,
-                    startDestination = Route.WELCOME
+                val scaffoldState = rememberScaffoldState()
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    scaffoldState = scaffoldState
                 ) {
-                    composable(Route.WELCOME) {
-                        WelcomeScreen { navController.navigate(it) }
-                    }
-                    composable(Route.AGE) {
+                    NavHost(
+                        navController = navController,
+                        startDestination = Route.WELCOME
+                    ) {
+                        composable(Route.WELCOME) {
+                            WelcomeScreen { navController.navigate(it) }
+                        }
+                        composable(Route.AGE) {
+                            AgeScreen(
+                                scaffoldState = scaffoldState,
+                                onNavigate = navController::navigate
+                            )
+                        }
+                        composable(Route.GENDER) {
+                            GenderScreen (onNavigate = {
+                                navController.navigate(it)
+                            })
+                        }
+                        composable(Route.HEIGHT) {
 
-                    }
-                    composable(Route.GENDER) {
-                        GenderScreen (onNavigate = {
-                            navController.navigate(it)
-                        })
-                    }
-                    composable(Route.HEIGHT) {
+                        }
+                        composable(Route.WEIGHT) {
 
-                    }
-                    composable(Route.WEIGHT) {
+                        }
+                        composable(Route.NUTRIENT_GOAL) {
 
-                    }
-                    composable(Route.NUTRIENT_GOAL) {
+                        }
+                        composable(Route.ACTIVITY) {
 
-                    }
-                    composable(Route.ACTIVITY) {
+                        }
+                        composable(Route.GOAL) {
 
-                    }
-                    composable(Route.GOAL) {
+                        }
+                        composable(Route.TRACKER_OVERVIEW) {
 
-                    }
-                    composable(Route.TRACKER_OVERVIEW) {
+                        }
+                        composable(Route.SEARCH) {
 
-                    }
-                    composable(Route.SEARCH) {
-
+                        }
                     }
                 }
             }
