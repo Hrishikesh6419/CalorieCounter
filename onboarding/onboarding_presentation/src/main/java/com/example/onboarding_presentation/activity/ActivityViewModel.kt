3029b132e4b4365d86a4c2eeb4427a168e0de1e0
Hrishikesh6419 <hrishikesh7165@gmail.com>
2022-02-02 23:44:36 -0500

    CC10 - Height Weight Screen (#11)
    Co-authored-by: Hrishikesh <hrishikesh.dharshan@citizensbank.com>

---
+++ onboarding/onboarding_presentation/src/main/java/com/example/onboarding_presentation/activity/ActivityViewModel.kt
@@ -0,0 +1,47 @@
+package com.example.onboarding_presentation.activity
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.core.domain.model.ActivityLevel
+import com.example.core.domain.model.Gender
+import com.example.core.domain.preferences.Preferences
+import com.example.core.navigation.Route
+import com.example.core.util.UiEvent
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ActivityViewModel @Inject constructor(
+    private val preferences: Preferences
+) : ViewModel() {
+
+    var selectedActivityLevel by mutableStateOf<ActivityLevel>(
+        ActivityLevel.Medium
+    )
+    private set
+    //We shouldn't be able to change this from outside
+
+    private val _uiEvent: Channel<UiEvent> = Channel<UiEvent>()
+    val uiEvent: Flow<UiEvent> = _uiEvent.receiveAsFlow()
+
+    fun onActivityLevelSelect(activityLevel: ActivityLevel) {
+        selectedActivityLevel = activityLevel
+    }
+
+    fun onNextClick() {
+        //Since sending an event into a channel is a suspending operation
+        viewModelScope.launch {
+            preferences.saveActivityLevel(selectedActivityLevel)
+            _uiEvent.send(UiEvent.Navigate(Route.GOAL))
+        }
+    }
+
+
+}
