16dafc99bdd88faeb22189983e3d544a84e32b9b
Hrishikesh <hrishikesh.dharshan@citizensbank.com>
2022-02-04 20:46:55 -0500

    CC15 - Tracker Repository and Mappers

---
+++ tracker/tracker_domain/src/main/java/com/example/tracker_domain/model/TrackedFood.kt
@@ -0,0 +1,18 @@
+package com.example.tracker_domain.model
+
+import java.time.LocalDate
+
+
+//We use to have this like an unified class for using to map
+data class TrackedFood(
+    val name: String,
+    val carbs: Int,
+    val protein: Int,
+    val fat: Int,
+    val imageUrl: String?,
+    val mealType: MealType,
+    val amount: Int,
+    val date: LocalDate,
+    val calories: Int,
+    val id: Int? = null
+)
