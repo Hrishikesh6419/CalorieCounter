6ba58e6a58f486aca135d4d70318f94a5d62e005
Hrishikesh <hrishikesh.dharshan@citizensbank.com>
2022-02-10 00:40:55 -0500

    CC16 - Tracker Usecases

--- onboarding/onboarding_domain/src/main/java/com/example/onboarding_domain/di/OnboardingDomainModule.kt
+++ onboarding/onboarding_domain/src/main/java/com/example/onboarding_domain/di/OnboardingDomainModule.kt
@@ -9,7 +9,7 @@ import dagger.hilt.android.scopes.ViewModelScoped
 
 @Module
 @InstallIn(ViewModelComponent::class)
-class OnboardingDomainModule {
+object OnboardingDomainModule {
 
     @Provides
     @ViewModelScoped
